9d96acf37fa101e52bfe8d9849d9f4da302f1ae3
Max Barnes <maxbarnes.dev@gmail.com>
2024-04-08 14:35:03 -0400

    fronted vue routes in progress

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{name: 'profile'}" v-if="$store.state.token !=''">Profile</router-link>
+      
     </div>
     <router-view />
   </div>
