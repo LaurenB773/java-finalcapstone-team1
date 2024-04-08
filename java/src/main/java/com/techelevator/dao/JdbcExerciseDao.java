b4cd2896c12b1f06816dad8951a1578a5712a4ec
Lauren <laurenbanks15021@gmail.com>
2024-04-08 14:40:12 -0400

    Equipment and Schedules are retrievable

--- java/src/main/java/com/techelevator/dao/JdbcExerciseDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcExerciseDao.java
@@ -1,4 +1,30 @@
 package com.techelevator.dao;
 
-public class JdbcExerciseDao {
+import com.techelevator.model.Exercise;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class JdbcExerciseDao implements ExerciseDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcExerciseDao(DataSource datasource) {
+        jdbcTemplate = new JdbcTemplate(datasource);
+    }
+    @Override
+    public Exercise startNewExercise() {
+        return null;
+    }
+
+    @Override
+    public List<Exercise> getAllUserExercisesByWorkoutId(int workoutId) {
+        return null;
+    }
+
+    @Override
+    public Exercise getExerciseById(int id) {
+        return null;
+    }
 }
--- java/src/main/java/com/techelevator/dao/JdbcScheduleDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcScheduleDao.java
@@ -1,12 +1,18 @@
 package com.techelevator.dao;
 
 
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Equipment;
 import com.techelevator.model.Schedule;
 import com.techelevator.model.UserProfile;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcScheduleDao implements ScheduleDao {
@@ -19,11 +25,32 @@ public class JdbcScheduleDao implements ScheduleDao {
 
     @Override
     public List<Schedule> getAllSchedules(){
-        return null;
+        List <Schedule> schedules = new ArrayList<>();
+        String sql = "select * from schedules";
+
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+            while (results.next()) {
+                schedules.add(mapToRowSchedules(results));
+            }
+        }catch (CannotGetJdbcConnectionException e){
+            throw new DaoException(e.getMessage());
+        }
+        return schedules;
     }
 
     @Override
     public Schedule getSchedule(int id) {
+        String sql = "select * from schedules where schedule_id = ?;";
+
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+            if (results.next()) {
+                return mapToRowSchedules(results);
+            }
+        }catch (CannotGetJdbcConnectionException e){
+            throw new DaoException(e.getMessage());
+        }
         return null;
     }
 
@@ -40,5 +67,19 @@ public class JdbcScheduleDao implements ScheduleDao {
     @Override
     public UserProfile createSchedule(Schedule newSchedule) {
         return null;
+    }
+    private Schedule mapToRowSchedules (SqlRowSet results){
+        Schedule schedule = new Schedule();
+        schedule.setScheduleId(results.getInt("schedule_id"));
+        schedule.setTitle(results.getString("title"));
+        schedule.setDescription(results.getString("description"));
+        LocalDate classDate = results.getDate("classDate").toLocalDate();
+        schedule.setDate(classDate);
+
+        // Retrieve time as LocalTime
+        LocalDate startTime = results.getDate("start_time").toLocalDate();
+        schedule.setTime(startTime);
+        schedule.setDuration(results.getInt("duration_minutes"));
+        return schedule;
     }
 }
