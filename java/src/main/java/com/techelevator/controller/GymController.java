686759daf4d4c87c28efba298f692d71c1574e13
Mihir Patel <mkpatel9702@gmail.com>
2024-04-08 11:35:27 -0400

    revised user profile table

--- java/src/main/java/com/techelevator/controller/GymController.java
+++ java/src/main/java/com/techelevator/controller/GymController.java
@@ -7,12 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.techelevator.dao.UserProfileDao;
 import com.techelevator.model.Equipment;
 import com.techelevator.model.UserProfile;
 
 @RestController
 @RequestMapping("/gym")
 public class GymController {
+
+  private UserProfileDao userProfileDao;
+  private EquipmentDao equipmentDao;
+  private ScheduleDao scheduleDao;
+
+  public GymController(UserProfileDao userProfileDao, EquipmentDao equipmentDao, ScheduleDao scheduleDao) {
+    this.userProfileDao = userProfileDao;
+    this.equipmentDao = equipmentDao;
+    this.scheduleDao = scheduleDao;
+  }
 
   @GetMapping("/equipment")
   public List<Equipment> getEquipment() {
@@ -22,7 +33,7 @@ public class GymController {
   @GetMapping("/members")
   public List<UserProfile> getMembers(Principal principal) {
     // check role
-    return userProfile.getMembers();
+    return userProfileDao.getMembers();
   }
 
   @GetMapping("/schedule")
