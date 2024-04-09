7b4744c1dfa0c93533643fd5d8a0af1c9d892618
Lauren <laurenbanks15021@gmail.com>
2024-04-09 13:10:22 -0400

    database modification

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -33,7 +33,7 @@ CREATE TABLE workouts (
   workout_id SERIAL PRIMARY KEY,
   start_time DATE NOT NULL,
   end_time DATE,
-  user_profile_id int REFERENCES user_profiles (user_profile_id),
+  user_profile_id int REFERENCES user_profiles (user_profile_id)
 );
 CREATE TABLE workout_exercises(
   workout_id int REFERENCES workouts (workout_id) NOT NULL,
