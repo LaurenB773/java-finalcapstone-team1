4d2be8fd534f9090a638c61daad4a2c381f6c0dc
gill <harnoorgill29@gmail.com>
2024-04-15 16:55:40 -0400

    checkin dao tests done

--- java/src/test/java/com/techelevator/dao/JdbcUserProfileDaoTests.java
+++ java/src/test/java/com/techelevator/dao/JdbcUserProfileDaoTests.java
@@ -8,16 +8,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class JdbcUserProfileDaoTests extends  BaseDaoTests{
  private JdbcUserProfileDao sut;
 
-    private static final User USER_1 = new User(1, "user1", "user1", "ROLE_USER");
-    private static final User USER_2 = new User(3, "employee", "employee", "ROLE_EMPLOYEE");
- private static final UserProfile PROFILE_1 = new UserProfile("name1","last1","user1@gmail.com","weight loss",1,1,null);
- private static final UserProfile PROFILE_2 = new UserProfile("name2","last2","user2@gmail.com","strength",2,2,null);
+ private static final UserProfile PROFILE_1 = new UserProfile("first1","last1","user1@gmail.com","weight loss",1,1,null);
+ private static final UserProfile PROFILE_2 = new UserProfile("first2","last2","user2@gmail.com","strength",2,2,null);
+ private static final UserProfile PROFILE_3 = new UserProfile("first3","last3","user3@gmail.com","body building",3,3,null);
     @Before
     public void setup() {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
@@ -27,14 +24,11 @@ public class JdbcUserProfileDaoTests extends  BaseDaoTests{
 
    @Test
     public void getProfile_give_valid_profile (){
-        UserProfile userProfile = sut.getProfile(1);
+        UserProfile userProfile1 = sut.getProfile(1);
+        assertUserProfileMatch(PROFILE_1,userProfile1);
 
-        Assert.assertEquals(PROFILE_1.getFirstName(),userProfile.getFirstName());
-        Assert.assertEquals(PROFILE_1.getLastName(),userProfile.getLastName());
-        Assert.assertEquals(PROFILE_1.getEmail(),userProfile.getEmail());
-        Assert.assertEquals(PROFILE_1.getGoal(),userProfile.getGoal());
-        Assert.assertEquals(PROFILE_1.getUserId(),userProfile.getUserId());
-        Assert.assertEquals(PROFILE_1.getProfileId(),userProfile.getProfileId());
+        UserProfile userProfile2 = sut.getProfile(2);
+        assertUserProfileMatch(PROFILE_2,userProfile2);
    }
    @Test
     public void createProfile_creates_a_profile(){
@@ -42,21 +36,41 @@ public class JdbcUserProfileDaoTests extends  BaseDaoTests{
        userProfile.setFirstName("first3");
        userProfile.setLastName("last3");
        userProfile.setEmail("user3@gmail.com");
-       userProfile.setGoal("weight loss");
+       userProfile.setGoal("body building");
+
 
        UserProfile createdProfile = sut.createProfile(userProfile, 3);
        Assert.assertNotNull(createdProfile);
+       assertUserProfileMatch(PROFILE_3,createdProfile);
    }
 
   @Test
   public void updateProfile_updates_profile(){
       UserProfile userProfile = new UserProfile();
-      userProfile.setFirstName("first3");
-      userProfile.setLastName("last3");
-      userProfile.setEmail("user3@gmail.com");
+      userProfile.setFirstName("first1");
+      userProfile.setLastName("last1");
+      userProfile.setEmail("user1@gmail.com");
       userProfile.setGoal("weight loss");
 
       sut.updateProfile(2,userProfile);
-
+      UserProfile updatedProfile = sut.getProfile(2);
+     assertUserProfileMatch(userProfile,updatedProfile);
   }
+
+  @Test
+    public void deleteProfile_deletes_profile(){
+        sut.deleteProfile(2);
+        UserProfile userProfile = sut.getProfile(2);
+        Assert.assertNull(userProfile);
+  }
+    private void assertUserProfileMatch(UserProfile expectedProfile, UserProfile actualProfile) {
+        Assert.assertEquals(expectedProfile.getFirstName(), actualProfile.getFirstName());
+        Assert.assertEquals(expectedProfile.getLastName(), actualProfile.getLastName());
+        Assert.assertEquals(expectedProfile.getEmail(), actualProfile.getEmail());
+        Assert.assertEquals(expectedProfile.getGoal(), actualProfile.getGoal());
+    }
+
+
+
+
 }
--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -6,7 +6,14 @@ INSERT INTO users (username,password_hash,role) VALUES ('user3','user3','ROLE_US
 
 
 INSERT INTO user_profiles (first_name, last_name, email, goal, user_id)
-VALUES ('name1', 'last1', 'user1@gmail.com', 'weight loss', 1),
-       ('name2', 'last2', 'user2@gmail.com', 'strength', 2);
+VALUES ('first1', 'last1', 'user1@gmail.com', 'weight loss', 1),
+       ('first2', 'last2', 'user2@gmail.com', 'strength', 2);
+
+INSERT INTO checkins (checkin_time, checkout_time)
+VALUES
+    (NOW(), NOW() + INTERVAL '1 HOUR'),
+    (NOW() - INTERVAL '1 DAY', NOW() - INTERVAL '1 DAY' + INTERVAL '2 HOUR');
+
+
 
 COMMIT TRANSACTION;
