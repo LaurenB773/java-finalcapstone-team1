32a34f3818a4473359b330911f9ca8ec80f84ac2
gill <harnoorgill29@gmail.com>
2024-04-17 11:07:23 -0400

    delete member can't login back and tests are almost done

--- java/src/test/java/com/techelevator/dao/JdbcExerciseDaoTests.java
+++ java/src/test/java/com/techelevator/dao/JdbcExerciseDaoTests.java
@@ -1,6 +1,8 @@
 package com.techelevator.dao;
 
 import com.techelevator.model.Exercise;
+import com.techelevator.security.model.User;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,10 +13,9 @@ import java.util.List;
 
 public class JdbcExerciseDaoTests extends BaseDaoTests{
     private JdbcExerciseDao sut;
-
-    private final Exercise EXERCISE_1 = new Exercise(1,"bench press",20, 12, 4 , 60);
-    private final Exercise EXERCISE_2 = new Exercise(2,"squat",18, 10, 3 , 70);
-    private final Exercise EXERCISE_3 = new Exercise(,"squat",18, 10, 3 , 70);
+    private final Exercise EXERCISE_1 = new Exercise(1,1,"bench press",20, 12, 4 , 60);
+    private final Exercise EXERCISE_2 = new Exercise(2,2,"squat",18, 10, 3 , 70);
+    private final Exercise EXERCISE_3 = new Exercise(3,1, "squat",18, 10, 3 , 70);
 
     @Before
     public void setup(){
@@ -25,7 +26,22 @@ public class JdbcExerciseDaoTests extends BaseDaoTests{
     @Test
     public void getExercise_returns_all_the_users_exercise(){
         List<Exercise> exercises = sut.getExercises(1);
+        Assert.assertEquals(2,exercises.size());
+    }
 
+    @Test
+    public void getExerciseById_returns_that_exercise() {
+        Exercise exercise = sut.getExerciseById(1);
+        Assert.assertEquals(EXERCISE_1.getExerciseName(),exercise.getExerciseName());
+        Assert.assertEquals(EXERCISE_1.getExerciseDurationMinutes(),exercise.getExerciseDurationMinutes());
+        Assert.assertEquals(EXERCISE_1.getWeightLbs(),exercise.getWeightLbs(),0.0);
+    }
+
+    @Test
+    public void deleteExercise_deletes_the_exercise() {
+       sut.deleteExercise(1);
+       Exercise exercise = sut.getExerciseById(1);
+       Assert.assertNull(exercise);
     }
 
 }
--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -15,7 +15,9 @@ VALUES ('barbell', 30 ), ('dumbbell', 50  ) ;
 
 INSERT INTO exercises (exercise_name, exercise_duration_minutes, sets, reps, weight_lbs)
 VALUES ('bench press', 20 , 4, 12 , 60),
-       ('squat', 18, 3, 10, 70);
-INSERT INTO user_exercise 
+       ('squat', 18, 3, 10, 70),
+       ('squat', 18, 10, 3 , 70);
+INSERT INTO user_exercises (user_id, exercise_id)
+VALUES (1, 1) , (2, 2) , (1, 3);
 
 COMMIT TRANSACTION;
